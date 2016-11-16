157196ad85afb25f75e93c1c1c8ee9eb3eb43e6c
Ryan Kanner <rkanner@medianewsgroup.com>
2016-11-16 12:36:29 -0700

    add the ability to pass multiple modifiers from a hook callback

--- includes/class-dfm-transient-hook.php
+++ includes/class-dfm-transient-hook.php
@@ -64,6 +64,33 @@ if ( ! class_exists( 'DFM_Transient_Hook' ) ) {
 		}
 
 		/**
+		 * Run an update in real time for the transient.
+		 *
+		 * @param string $modifier Name of the modifier
+		 * @return void
+		 * @access private
+		 */
+		private function run_update( $modifier ) {
+
+			$transient_obj = new DFM_Transients( $this->transient, $modifier );
+
+			// Bail if another process is already trying to update this transient.
+			if ( $transient_obj->is_locked() && ! $transient_obj->owns_lock( '' ) ) {
+				return;
+			}
+
+			if ( ! $transient_obj->is_locked() ) {
+				$transient_obj->lock_update();
+			}
+
+			$data = call_user_func( $transient_obj->transient_object->callback, $modifier );
+			$transient_obj->set( $data );
+
+			$transient_obj->unlock_update();
+
+		}
+
+		/**
 		 * Spawn a process to regenerate the transient data
 		 *
 		 * @return void
@@ -71,7 +98,7 @@ if ( ! class_exists( 'DFM_Transient_Hook' ) ) {
 		 */
 		public function spawn() {
 
-			$modifier = '';
+			$modifiers = '';
 
 			if ( ! empty( $this->callback ) ) {
 
@@ -81,32 +108,37 @@ if ( ! class_exists( 'DFM_Transient_Hook' ) ) {
 				// Call the callback for this hook, and pass the args to it.
 				// This callback decides if we should actually run the process to update the transient data based on the
 				// args passed to it. The callback should return false if we don't want to run the action, and should
-				// return the transient modifier if we do want to run it.
-				$modifier = call_user_func( $this->callback, $hook_args );
-				if ( false === $modifier ) {
+				// return the transient modifier if we do want to run it. You can also optionally return an array of
+				// modifiers if you want to update multiple transients at once.
+				$modifiers = call_user_func( $this->callback, $hook_args );
+				if ( false === $modifiers ) {
 					return;
 				}
 			}
 
+			// If we are running an async task, instantiate a new async handler.
 			if ( $this->async ) {
-				new DFM_Async_Handler( $this->transient, $modifier );
+
+				// If we have an array of modifiers, update each of them.
+				if ( is_array( $modifiers ) ) {
+					foreach ( $modifiers as $modifier ) {
+						new DFM_Async_Handler( $this->transient, $modifier );
+					}
+				} else {
+					new DFM_Async_Handler( $this->transient, $modifiers );
+				}
+
+			// Run the update in real time if we aren't using an async process
 			} else {
 
-				$transient_obj = new DFM_Transients( $this->transient, $modifier );
-
-				// Bail if another process is already trying to update this transient.
-				if ( $transient_obj->is_locked() && ! $transient_obj->owns_lock( '' ) ) {
-					return;
+				// If we have an array of modifiers, update each of them.
+				if ( is_array( $modifiers ) ) {
+					foreach ( $modifiers as $modifier ) {
+						$this->run_update( $modifier );
+					}
+				} else {
+					$this->run_update( $modifiers );
 				}
-
-				if ( ! $transient_obj->is_locked() ) {
-					$transient_obj->lock_update();
-				}
-
-				$data = call_user_func( $transient_obj->transient_object->callback, $modifier );
-				$transient_obj->set( $data );
-
-				$transient_obj->unlock_update();
 
 			}
 

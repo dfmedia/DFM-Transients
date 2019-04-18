81c96aca379a220dbb1f3b3da96df6e1e993bb0b
CodeProKid <ryan.kanner@gmail.com>
2019-04-18 16:04:19 -0600

    Adding tests to support new features

--- tests/test-class-dfm-transient-scheduler.php
+++ tests/test-class-dfm-transient-scheduler.php
@@ -1,6 +1,25 @@
 <?php
 class Test_Class_DFM_Transient_Scheduler extends WP_UnitTestCase {
 
+	protected $server;
+	protected $endpoint;
+
+	public function setUp() {
+
+		parent::setup();
+
+		global $wp_rest_server;
+		$wp_rest_server = new \WP_REST_Server;
+		$this->server   = $wp_rest_server;
+		$this->endpoint = '/' . DFM_Transient_Scheduler::API_NAMESPACE . '/' . DFM_Transient_Scheduler::ENDPOINT_RUN . '/(?P<transient>[\w|-]+)';
+		do_action( 'rest_api_init' );
+
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+	}
+
 	/**
 	 * Don't setup any hooks if no transients are registered
 	 */
@@ -22,6 +41,197 @@ class Test_Class_DFM_Transient_Scheduler extends WP_UnitTestCase {
 
 		$actual = $scheduler_obj->get_transients();
 		$this->assertNull( $actual );
+
+	}
+
+	/**
+	 * Test that our endpoint actually exists.
+	 */
+	public function testEndpointRegistered() {
+
+		$scheduler_obj = new DFM_Transient_Scheduler();
+		$scheduler_obj->register_rest_endpoint();
+
+		$this->assertTrue( isset( $this->server->get_routes()[ $this->endpoint ] ) );
+		$this->assertTrue( isset( $this->server->get_routes()[ '/' . DFM_Transient_Scheduler::API_NAMESPACE ] ) );
+
+	}
+
+	/**
+	 * Test the endpoint returns an error when trying to be accessed
+	 */
+	public function testInvalidRequest() {
+
+		$scheduler_obj = new DFM_Transient_Scheduler();
+		$scheduler_obj->register_rest_endpoint();
+
+		$request = new WP_REST_Request( 'GET', '/' . DFM_Transient_Scheduler::API_NAMESPACE . '/' . DFM_Transient_Scheduler::ENDPOINT_RUN . '/test' );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 404, $response->data['data']['status'] );
+
+	}
+
+	/**
+	 * Test that processing doesn't happen if auth fails
+	 */
+	public function testAuthFailure() {
+
+		$scheduler_obj = new DFM_Transient_Scheduler();
+		$scheduler_obj->register_rest_endpoint();
+
+		$request = new WP_REST_Request( 'PUT', '/' . DFM_Transient_Scheduler::API_NAMESPACE . '/' . DFM_Transient_Scheduler::ENDPOINT_RUN . '/test' );
+		$request->set_body( wp_json_encode( [
+			'secret' => 'test',
+		] ) );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 'no-secret', $response->data['code'] );
+
+	}
+
+	/**
+	 * Test that the request executes properly
+	 */
+	public function testSuccessfulRequest() {
+
+		$transient_name = 'testSuccessfulRequest';
+
+		dfm_register_transient( $transient_name, [
+			'callback' => function( $modifier ) {
+				return 'modifier: ' . $modifier;
+			},
+			'async' => true,
+		] );
+
+		$scheduler_obj = new DFM_Transient_Scheduler();
+		$scheduler_obj->register_rest_endpoint();
+
+		$request = new WP_REST_Request( 'PUT', '/' . DFM_Transient_Scheduler::API_NAMESPACE . '/' . DFM_Transient_Scheduler::ENDPOINT_RUN . '/' . $transient_name );
+		$request->set_body( wp_json_encode( [
+			'secret' => DFM_TRANSIENTS_SECRET,
+			'modifiers' => 'test',
+			'lock_key' => '',
+		] ) );
+
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( $transient_name . ' transient updated', $response->data );
+		$this->assertEquals( 200, $response->status );
+
+		$expected = 'modifier: test';
+		$actual = get_transient( $transient_name . '_test' );
+		$this->assertEquals( $expected, $actual );
+
+	}
+
+	/**
+	 * Test that transients with multiple modifiers process properly
+	 */
+	public function testSuccessfulRequestWithMultipleModifiers() {
+
+		$transient_name = 'testSuccessfulRequestWithMultipleModifiers';
+
+		dfm_register_transient( $transient_name, [
+			'callback' => function( $modifier ) {
+				return 'modifier: ' . $modifier;
+			},
+			'async' => true,
+		] );
+
+		$scheduler_obj = new DFM_Transient_Scheduler();
+		$scheduler_obj->register_rest_endpoint();
+
+		$request = new WP_REST_Request( 'PUT', '/' . DFM_Transient_Scheduler::API_NAMESPACE . '/' . DFM_Transient_Scheduler::ENDPOINT_RUN . '/' . $transient_name );
+		$request->set_body( wp_json_encode( [
+			'secret' => DFM_TRANSIENTS_SECRET,
+			'modifiers' => [ 'foo', 'bar' ],
+			'lock_key' => '',
+		] ) );
+
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( $transient_name . ' transient updated', $response->data );
+		$this->assertEquals( 200, $response->status );
+
+		$expected = 'modifier: foo';
+		$actual = get_transient( $transient_name . '_foo' );
+		$this->assertEquals( $expected, $actual );
+
+		$expected = 'modifier: bar';
+		$actual = get_transient( $transient_name . '_bar' );
+		$this->assertEquals( $expected, $actual );
+
+	}
+
+	/**
+	 * Test that updates get spawned appropriately
+	 */
+	public function testUpdatesSpawnedCorrectly() {
+
+		$transient_name = 'testUpdatesSpawnedCorrectly';
+
+		dfm_register_transient( $transient_name, [
+			'callback' => function( $modifier ) {
+				return 'modifier: ' . $modifier;
+			},
+			'update_hooks' => [
+				'my_test_update_hook' => function() {
+					return [ 'foo', 'bar' ];
+				}
+			],
+			'async' => true,
+		] );
+
+		$scheduler_obj = new DFM_Transient_Scheduler();
+		$scheduler_obj->get_transients();
+
+		do_action( 'my_test_update_hook' );
+
+		$scheduler_obj->execute_async_updates();
+
+		global $wp_filter;
+		$this->assertEquals( 3, count( $wp_filter['shutdown'][10] ) );
+
+	}
+
+	/**
+	 * Test that hooks get added corrected
+	 */
+	public function testHooksGetScheduled() {
+
+		$transient_name = 'testHooksGetScheduled';
+		dfm_register_transient( $transient_name, [
+			'callback' => function( $modifier ) {
+				return 'modifier: ' . $modifier;
+			},
+			'update_hooks' => 'my_test_hook',
+		] );
+
+		$scheduler_obj = new DFM_Transient_Scheduler();
+		$scheduler_obj->get_transients();
+
+		$this->assertTrue( has_action( 'my_test_hook' ) );
+
+	}
+
+	/**
+	 * Test that multiple update hooks registered to a transient get added properly
+	 */
+	public function testMultipleHooksGetScheduled() {
+
+		$transient_name = 'testMultipleHooksGetScheduled';
+		dfm_register_transient( $transient_name, [
+			'callback' => function( $modifier ) {
+				return 'modifier: ' . $modifier;
+			},
+			'update_hooks' => [
+				'foo' => '__return_true',
+				'bar' => '__return_true',
+			]
+		] );
+
+		$scheduler_obj = new DFM_Transient_Scheduler();
+		$scheduler_obj->get_transients();
+
+		$this->assertTrue( has_action( 'foo' ) );
+		$this->assertTrue( has_action( 'bar' ) );
 
 	}
 
@@ -84,11 +294,8 @@ class Test_Class_DFM_Transient_Scheduler extends WP_UnitTestCase {
 		 * Manually lock the transient from updating
 		 */
 		set_transient( 'dfm_lt_' . $transient_name . '_1', 'somevalue' );
-
 		$scheduler_obj = new DFM_Transient_Scheduler();
-
 		$scheduler_obj->run_update( $transient_name, '1', '' );
-
 		$this->assertFalse( get_transient( $transient_name . '_1' ) );
 
 	}
